0e1f4859f647feec4222aa87c7121efbac8dc613
laem99 <c9laem00@students.oamk.fi>
2021-12-08 11:35:31 +0200

    11.12.2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('v2') // change this string to ensure a new version deployed
+  res.send('v6') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT)
