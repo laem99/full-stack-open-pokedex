890127e4a8b96571acb6b9a569620e9a3be5de36
laem99 <c9laem00@students.oamk.fi>
2021-12-08 09:22:13 +0200

    11.8.1

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -21,7 +21,7 @@ const PokemonPage = ({ previous, next }) => {
   const { type } = pokemon.types.find((type) => type.slot === 1)
   const stats = pokemon.stats.map((stat) => ({
     name: formatName(stat.stat.name),
-    value: stat.base_stat
+    value: stat.base_stat,
   })).reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
@@ -31,7 +31,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
